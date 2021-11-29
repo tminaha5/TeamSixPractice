635389026dcdac960e018aec003efd8071a7a102
sajia <sajia@10.0.0.77>
2021-11-28 23:19:07 -0500

    I added comment

--- src/six/practice/PracticeDemo.java
+++ src/six/practice/PracticeDemo.java
@@ -3,6 +3,7 @@ package six.practice;
 public class PracticeDemo {
 	
 	public static void main(String [] args) {
+		System.out.println("This is our second practice");
 		
 		System.out.println("Homework Practice for Team 6");
 		System.out.println("Great job");
